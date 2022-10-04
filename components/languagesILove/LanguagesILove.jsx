e8dfd9fdd411447f73d7bf856ec86d2f4b55a608
Andres Ausecha Mosquera <andres@aamsis.co>
2022-10-03 19:55:04 -0500

    separate code in a new section

---
+++ components/languagesILove/LanguagesILove.jsx
@@ -0,0 +1,13 @@
+export const LanguagesILove = () => (
+  <>
+    <h4 style={{ textAlign: 'center', color: 'white' }}>
+      Languages I love
+    </h4>
+    <div style={{ display: 'flex', justifyContent: 'center', padding: '1rem' }}>
+      <img width="100px" height="100px" src="/images/java.png" alt="" />
+      <img width="100px" height="100px" src="/images/javascript.png" alt="" />
+      <img width="100px" height="100px" src="/images/node.png" alt="" />
+      <img width="100px" height="100px" src="/images/ts.jpg" alt="" />
+    </div>
+  </>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { TextRenderer } from '../components/textRenderer/TextRenderer';
 import { Avatar } from '../components/intro/IntroToMe';
 import { Navbar } from '../components/navbar/Navbar';
+import { LanguagesILove } from '../components/languagesILove/LanguagesILove';
 
 export default function Home() {
   return (
@@ -10,17 +11,10 @@ export default function Home() {
       <Avatar />
       <br />
       <TextRenderer
-        text="Hi, I am Andres Ausecha, systems and software engineer from Colombia National University. I am also
+        text="Hi, I am Andres Ausecha, Systems and software engineer from Colombia National University. I am also
         Fullstack web developer, highly passionate about technology, languages, development patterns, and user functionality."
       />
-      <div style={{ height: "3rem" }} />
-        <div style={{ display: 'flex', justifyContent: 'center', backgroundColor: 'white', padding: '1rem' }}>
-          <img width="100px" height="100px" src="/images/java.png" alt="" />
-          <img width="100px" height="100px"  src="/images/javascript.png" alt="" />
-          <img width="100px" height="100px" src="/images/node.png" alt="" />
-          <img width="100px" height="100px"  src="/images/ts.jpg" alt="" />
-        </div>
-        <div style={{ height: "3rem" }} />
+      <LanguagesILove />
       </div>
     </div>
   );
