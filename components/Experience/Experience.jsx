95835f62712719fd591e8766f8a90087fdf4f3db
Andres Ausecha Mosquera <andres@aamsis.co>
2022-10-04 18:22:49 -0500

    add description for MLREEF experience

--- components/Experience/Experience.jsx
+++ components/Experience/Experience.jsx
@@ -1,4 +1,5 @@
 import styles from '../../styles/Experience.module.scss';
+import { TextRenderer } from '../textRenderer/TextRenderer';
 
 export const Experience = () => (
   <div className={styles.experience}>
@@ -8,6 +9,11 @@ export const Experience = () => (
     <h4>
       MLREEF
     </h4>
+    <TextRenderer 
+      text="In this company we develop a platform for machine learning development, as a fulstack developer, 
+      my responsability consists to create, maintain and test the frontend implementation, as well as collaborate 
+      in the backend side and insfrastructure time to time."
+    />
     <a href="https://www.mlreef.com/" target="_blank" rel="noopener noreferrer">
       <img src="/images/mlreef1.png" width="60vw" />
     </a>
