423d6306399340dc377cf9e574114110cb51a684
Andres Ausecha Mosquera <andres@aamsis.co>
2022-10-04 21:56:49 -0500

    creae new page to add tutorials

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,15 +1,30 @@
-import { useState } from 'react';
 import styles from '../../styles/Navbar.module.scss'
+import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 export const Navbar = () => {
-  const [active, setActive] = useState("home");
+  const router = useRouter();
   return (
   <div className={styles.navbar}>
     <ul>
-      <li onClick={() => { setActive("home") }} style={{ backgroundColor: active === "home" ? "#13ecff" : "" }}><button style={{background:'none' }} >Home</button></li>
-    {/*   <li onClick={() => { setActive("news") }} style={{ backgroundColor: active === "news" ? "#13ecff" : "" }}><button style={{background:'none' }} >News</button></li>
+      <li style={{ backgroundColor: router.asPath === "/" ? "#13ecff" : "" }}>
+        <Link
+          href={"/"}
+        >
+          Home
+        </Link>
+      </li>
+      <li style={{ backgroundColor: router.asPath === "/tutorials" ? "#13ecff" : "" }}>
+        <Link
+          href={"/tutorials"}
+        >
+            Tutorials
+        </Link>
+      </li>
+      {/*
       <li onClick={() => { setActive("contact") }} style={{ backgroundColor: active === "contact" ? "#13ecff" : "" }}><button style={{background:'none' }} >Contact</button></li>
-      <li onClick={() => { setActive("about") }} style={{ backgroundColor: active === "about" ? "#13ecff" : "" }}><button style={{background:'none' }} >About</button></li> */}
+      <li onClick={() => { setActive("about") }} style={{ backgroundColor: active === "about" ? "#13ecff" : "" }}><button style={{background:'none' }} >About</button></li> 
+      */}
     </ul> 
   </div>
 )};
---
+++ pages/tutorials/tutorials.jsx
@@ -0,0 +1,20 @@
+import { Navbar } from "../../components/navbar/Navbar";
+import styles from '../../styles/Tutorials.module.scss';
+
+export const Tutorials = () => {
+  return (
+    <>
+    <Navbar />
+    <div className="parallax">
+      <div className={styles.tutorials}>
+        <h2>
+          Tutorials page
+        </h2>
+        <p>
+          content will be added soon...
+        </p>
+      </div>
+    </div>
+    </>
+  )
+}
