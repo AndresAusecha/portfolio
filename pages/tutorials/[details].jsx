1ebfdec528cbc403a97af01cf983b114e6b86261
Andres Ausecha Mosquera <andres@aamsis.co>
2022-10-26 16:01:47 -0500

    render tutorial content

--- pages/tutorials/[details].jsx
+++ pages/tutorials/[details].jsx
@@ -1,13 +1,38 @@
 import { useRouter } from "next/router";
+import { tutorialData } from "./data";
 
 const TutorialDetails = () => {
   const router = useRouter();
-  console.log(router.query);
+  const { details } = router.query;
+  const tutorialContent = tutorialData.find((tuto) => tuto.id === details);
   return (
     <div>
-      <h1>
-        TutorialDetails
-      </h1>
+      <h2>
+        {tutorialContent.title}
+      </h2>
+      <div>
+        {tutorialContent.content.map((contentLine, ind) => {
+          if (contentLine.startsWith("p/")) {
+            return (
+              <p key={ind}>
+                {contentLine.substring(2, contentLine.length - 3)}
+              </p>
+            )
+          } else  if (contentLine.startsWith("h3/")) {
+            return (
+              <h3 key={ind}>
+                {contentLine.substring(3, contentLine.length - 3)}
+              </h3>
+            )
+          }
+          else  if (contentLine.startsWith("img/")) {
+            return (
+              <img key={ind} src={`/images/${contentLine.substring(3, contentLine.length - 3)}`} />
+            )
+          }
+        }
+        )}
+      </div>
     </div>
   )
 }
